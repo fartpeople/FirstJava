8ae5d844f24594c6f42c90d5d3f2eafc7145598f
fartpeople <105021080@live.asia.edu.tw>
2016-09-21 11:43:04 +0800

    My First Java Program

---
+++ src/test.java
@@ -0,0 +1,16 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+		String nickname = "資工人";
+		String uni = "亞洲大學";
+		System.out.println(nickname);
+		System.out.print(uni + "\n");
+		System.out.println(nickname + "   " + uni);
+	}
+
+}
